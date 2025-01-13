8c02293e915935fc12fa6824965f256e78cf4ac9
rutmanTanya <t.rutman13@gmail.com>
2025-01-13 19:11:02 +0200

    Update app.js

--- Task1/app.js
+++ Task1/app.js
@@ -1,7 +1,6 @@
 /*Submitted by: Tanya Rotman, Alex Lapin
 Class 48/6
-Exercise 7
-Work on list of users and their details
+Exercise 1
 */
 
 
@@ -9,33 +8,35 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Middleware 1: Add "Hello1"
+// Middleware 1: Console "Hello1"
 app.use((req, res, next) => {
     console.log('Hello1');
     next();
 });
-// Middleware 2: Add "Hello2"
+// Middleware 2: Console "Hello2"
 app.use((req, res, next) => {
     console.log('Hello2');
     next();
 });
+
 // Route for "/users"
 app.get('/users/*', (req, res) => {
     res.send(`
       <html>
         <body>
-          <h1>Users Route</h1>
+          <h1>Users</h1>
           <p>This is the /users route!</p>
         </body>
       </html>
     `);
 });
+
 // Route for other
 app.get('*', (req, res) => {
     res.send(`
       <html>
         <body>
-          <h1>Other Route</h1>
+          <h1>Others</h1>
           <p>This is a route other than /users!</p>
         </body>
       </html>
